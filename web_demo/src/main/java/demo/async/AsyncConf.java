0c9f64db29e36ea5ad1bb8228decb8f5c6da739c
jian.jiang <jian.jiang@adicon.com.cn>
2022-01-08 03:18:05 +0900

    AsyncConf

--- web_demo/src/main/java/demo/async/AsyncConf.java
+++ web_demo/src/main/java/demo/async/AsyncConf.java
@@ -3,11 +3,16 @@ package demo.async;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 
 import java.util.Arrays;
 import java.util.concurrent.Executor;
@@ -19,12 +24,15 @@ public class AsyncConf implements AsyncConfigurer {
 
     /**
      * 注入异步线程使用的线程池
+     *
      * @return 线程池
      */
+    @Lazy
     @Bean
     public Executor cpuPool() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setThreadNamePrefix("Async-Pool");
+        executor.setTaskDecorator(createTaskDecorator());
         executor.setCorePoolSize(10);
         executor.setMaxPoolSize(10);
         executor.setQueueCapacity(2000);
@@ -35,6 +43,7 @@ public class AsyncConf implements AsyncConfigurer {
 
     /**
      * 异步线程异常处理
+     *
      * @return 异常handler
      */
     @Override
@@ -45,4 +54,18 @@ public class AsyncConf implements AsyncConfigurer {
                     Arrays.toString(params), ex);
         };
     }
+
+
+    public TaskDecorator createTaskDecorator() {
+        return runnable -> () -> {
+            RequestAttributes context = RequestContextHolder.currentRequestAttributes();
+            try {
+                RequestContextHolder.setRequestAttributes(context);
+                runnable.run();
+            } finally {
+                RequestContextHolder.resetRequestAttributes();
+            }
+        };
+    }
+
 }
