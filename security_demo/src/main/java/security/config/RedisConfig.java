6bbb56e432e8397b92a3e2bc1b3d03a2d85bc8e3
jian.jiang <jian.jiang@adicon.com.cn>
2022-09-13 16:48:35 +0900

    stream collect merge  函数应用

---
+++ security_demo/src/main/java/security/config/RedisConfig.java
@@ -0,0 +1,235 @@
+package security.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizers;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.autoconfigure.cache.CacheProperties.Redis;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheOperationInvocationContext;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.lang.Nullable;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableCaching
+@EnableConfigurationProperties({CacheProperties.class})
+@RequiredArgsConstructor
+public class RedisConfig extends CachingConfigurerSupport {
+
+    final Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder;
+    final JacksonProperties jacksonProperties;
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public CacheManagerCustomizers cacheManagerCustomizers(ObjectProvider<CacheManagerCustomizer<?>> customizers) {
+        return new CacheManagerCustomizers(customizers.orderedStream().collect(Collectors.toList()));
+    }
+
+    @Bean
+    @Primary
+    public CacheManager caffeine() {
+
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(Caffeine.newBuilder().softValues().expireAfterWrite(Duration.ofMinutes(2))
+                .initialCapacity(1000).maximumSize(100000));
+        return caffeineCacheManager;
+    }
+
+    @Bean
+    CacheManager redis(CacheProperties cacheProperties, CacheManagerCustomizers cacheManagerCustomizers,
+                       ObjectProvider<org.springframework.data.redis.cache.RedisCacheConfiguration> redisCacheConfiguration,
+                       ObjectProvider<RedisCacheManagerBuilderCustomizer> redisCacheManagerBuilderCustomizers,
+                       RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(
+                determineConfiguration(cacheProperties, redisCacheConfiguration));
+        List<String> cacheNames = cacheProperties.getCacheNames();
+        if (!cacheNames.isEmpty()) {
+            builder.initialCacheNames(new LinkedHashSet<>(cacheNames));
+        }
+        redisCacheManagerBuilderCustomizers.orderedStream().forEach((customizer) -> customizer.customize(builder));
+        return cacheManagerCustomizers.customize(builder.build());
+    }
+
+    private org.springframework.data.redis.cache.RedisCacheConfiguration determineConfiguration(
+            CacheProperties cacheProperties,
+            ObjectProvider<org.springframework.data.redis.cache.RedisCacheConfiguration> redisCacheConfiguration) {
+        return redisCacheConfiguration.getIfAvailable(() -> determineConfiguration(cacheProperties));
+    }
+
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(jacksonProperties.getTimeZone());
+    }
+
+    public RedisCacheConfiguration determineConfiguration(CacheProperties cacheProperties) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+        // 设置redis中值的序列化方式，方便redisClient可读
+        config = config.serializeValuesWith(SerializationPair.fromSerializer(jackson2JsonRedisSerializer()))
+                .computePrefixWith(name -> name + ":");
+        Redis redisProperties = cacheProperties.getRedis();
+        if (redisProperties.getTimeToLive() != null) {
+            config = config.entryTtl(redisProperties.getTimeToLive());
+        }
+        if (redisProperties.getKeyPrefix() != null) {
+            config = config.prefixCacheNameWith(redisProperties.getKeyPrefix());
+        }
+        if (!redisProperties.isCacheNullValues()) {
+            config = config.disableCachingNullValues();
+        }
+        if (!redisProperties.isUseKeyPrefix()) {
+            config = config.disableKeyPrefix();
+        }
+        return config;
+    }
+
+    /**
+     * 使用Jackson序列化器
+     */
+    private RedisSerializer<Object> jackson2JsonRedisSerializer() {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(
+                Object.class);
+
+        jackson2JsonRedisSerializer.setObjectMapper(buildMapper());
+        return jackson2JsonRedisSerializer;
+    }
+
+    private ObjectMapper buildMapper() {
+        ObjectMapper objectMapper = jackson2ObjectMapperBuilder.createXmlMapper(false).build();
+
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+
+        //设置类型
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY);
+        objectMapper.registerModule(new JavaTimeModule());
+
+        //忽略null值
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper;
+    }
+
+
+    @Bean
+    @Primary
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+        template.setDefaultSerializer(genericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+
+    private RedisSerializer<Object> genericJackson2JsonRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer(buildMapper());
+    }
+
+    @Override
+    @Nullable
+    public CacheErrorHandler errorHandler() {
+        return new IgnoreExceptionCacheErrorHandler();
+    }
+
+    @Bean
+    public CacheResolver multiCache(CacheManager redis, CacheManager caffeine) {
+
+        return new MultiCache(caffeine, redis);
+
+    }
+
+    @Slf4j
+    public static class IgnoreExceptionCacheErrorHandler implements CacheErrorHandler {
+
+        @Override
+        public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+            log.error(exception.getMessage(), exception);
+        }
+
+        @Override
+        public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+            log.error(exception.getMessage(), exception);
+        }
+
+        @Override
+        public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+            log.error(exception.getMessage(), exception);
+        }
+
+        @Override
+        public void handleCacheClearError(RuntimeException exception, Cache cache) {
+            log.error(exception.getMessage(), exception);
+        }
+    }
+
+
+    public static class MultiCache implements CacheResolver {
+
+
+        final CacheManager[] cacheManagers;
+
+        public MultiCache(CacheManager... cacheManagers) {
+            this.cacheManagers = cacheManagers;
+        }
+
+        @Override
+        public Collection<? extends Cache> resolveCaches(CacheOperationInvocationContext<?> context) {
+
+            Collection<String> cacheNames = context.getOperation().getCacheNames();
+
+            Collection<Cache> result = new ArrayList<>(cacheNames.size());
+
+            for (String cacheName : cacheNames) {
+
+                List<Cache> collect = Arrays.stream(cacheManagers).map(e -> e.getCache(cacheName)).collect(Collectors.toList());
+                result.addAll(collect);
+                if (result.size() == 0) {
+                    throw new IllegalArgumentException("Cannot find cache named '" +
+                            cacheName + "' for " + context.getOperation());
+                }
+            }
+            return result;
+        }
+
+    }
+
+}
---
+++ security_demo/src/main/java/security/config/SqlPrint.java
@@ -0,0 +1,47 @@
+package security.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.log4j2.Log4j2LoggerImpl;
+
+@Slf4j
+public class SqlPrint implements Log {
+    public SqlPrint(String clazz) {
+        // Do Nothing
+    }
+
+    @Override
+    public boolean isDebugEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isTraceEnabled() {
+        return true;
+    }
+
+    @Override
+    public void error(String s, Throwable e) {
+        log.error(s, e);
+    }
+
+    @Override
+    public void error(String s) {
+        log.error(s);
+    }
+
+    @Override
+    public void debug(String s) {
+        log.info(s);
+    }
+
+    @Override
+    public void trace(String s) {
+        log.trace(s);
+    }
+
+    @Override
+    public void warn(String s) {
+        log.warn(s);
+    }
+}
